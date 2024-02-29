a9db1ec6c14e3d8b05d761345f3c406869ef07a6
GuillotSamuel <samuelguillot75@gmail.com>
2024-02-29 14:31:18 +0100

    test merger

--- src/pipex/pipex.c
+++ src/pipex/pipex.c
@@ -1,24 +1,31 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   parsing.c                                          :+:      :+:    :+:   */
+/*   pipex.c                                            :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: sguillot <sguillot@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/02/07 19:12:02 by sguillot          #+#    #+#             */
-/*   Updated: 2024/02/29 14:29:07 by sguillot         ###   ########.fr       */
+/*   Created: 2024/02/29 14:05:57 by sguillot          #+#    #+#             */
+/*   Updated: 2024/02/29 14:10:15 by sguillot         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../includes/minishell.h"
 
-void	parsing(char *line, t_data *data)
+static void	ft_cmd_counter(t_data *data)
 {
-	if (token(line, data) == ERROR_G)
-		return ;
-	print_token(data);
-	expand_all_tokens(data);
-	// a suppr
-	//print_token(data);
-	clear_lists(data);
+	t_cmd_line	*cmd_list_dup;
+
+	data->cmd_counter = 0;
+	cmd_list_dup = data->cmd_list;
+	while (cmd_list_dup)
+	{
+		data->cmd_counter += 1;
+		cmd_list_dup = cmd_list_dup->next;
+	}
+}
+
+void	pipex(t_data *data)
+{
+	ft_cmd_counter(data);
 }
