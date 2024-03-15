5772e340ce700141785d14fbc80f9ab0f19486e9
GuillotSamuel <samuelguillot75@gmail.com>
2024-03-15 11:55:52 +0100

    adding : branch

--- src/exec/core_exec/forking_exec.c
+++ src/exec/core_exec/forking_exec.c
@@ -6,25 +6,24 @@
 /*   By: sguillot <sguillot@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/14 15:41:20 by sguillot          #+#    #+#             */
-/*   Updated: 2024/03/14 19:55:10 by sguillot         ###   ########.fr       */
+/*   Updated: 2024/03/15 11:44:25 by sguillot         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../../includes/minishell.h"
 
-static void	command_or_builtin(t_data *data)
+static void	command_or_builtin(t_data *data, t_cmd_line *cmd_list)
 {
 	t_cmd_line	*cmd_list_dup;
 	char		*path;
 	
-	cmd_list_dup = data->cmd_list;
+	cmd_list_dup = cmd_list;
 	if (check_builtin(cmd_list_dup->args[0]) == 1)
 	{
-		printf("BUILTIN\n");
 		fflush(stdout);
 		exec_builtin(cmd_list_dup, data);
-		return ; // TO REMOVE WHEN FORKING IS DONE
-		//exit(NULL); // TO ADD WHEN FORKING IS DONE
+		free_all(data);
+		exit(0); // TO ADD WHEN FORKING IS DONE
 	}
 	path = ft_cmd_exist(cmd_list_dup->token_list->token);
 	if (path != VAR_NOT_FOUND)
@@ -35,9 +34,33 @@ static void	command_or_builtin(t_data *data)
 
 void	forking_exec(t_data *data)
 {
-	if (data->cmd_list->redir->fd_in != 0)
-		dup2(data->cmd_list->redir->fd_in, 0);
-	if (data->cmd_list->redir->fd_out != 1)
-		dup2(data->cmd_list->redir->fd_out, 1);
-	command_or_builtin(data);
+	t_cmd_line	*cmd_list;
+
+	cmd_list = data->cmd_list;
+	int		i;
+	int		num_children;
+	pid_t	pid;
+	
+	i = 0;
+	num_children = cmd_nb(data->cmd_list);
+	while (i < num_children)
+	{
+		pid = fork();
+		if (pid < 0)
+		{
+			free_all(data);
+		}
+		else if (pid == 0)
+		{
+			//redirect_io(data);
+			command_or_builtin(data, cmd_list);
+			//exit (0);
+		}
+		else if (pid > 0)
+		{
+			wait(NULL);
+		}
+		i++;
+		cmd_list = cmd_list->next;
+	}
 }
