1afbfd34d6e05e1e35d3106631c3f30911d757c2
GuillotSamuel <samuelguillot75@gmail.com>
2024-03-17 17:44:41 +0100

    correction

--- src/parsing/token/line_control/line_ctrl_1.c
+++ src/parsing/token/line_control/line_ctrl_1.c
@@ -6,7 +6,7 @@
 /*   By: sguillot <sguillot@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/07 19:31:19 by sguillot          #+#    #+#             */
-/*   Updated: 2024/03/17 16:17:39 by sguillot         ###   ########.fr       */
+/*   Updated: 2024/03/17 17:43:41 by sguillot         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -67,8 +67,8 @@ int	line_ctrl(char *line)
 		|| first_char_ctrl(line) == ERROR_SYNTAX
 		|| last_char_ctrl(line) == ERROR_SYNTAX
 		|| consecutive_pipes_ctrl(line) == ERROR_SYNTAX
-		// || forbiden_consecutive(line) == ERROR_SYNTAX
+		//|| forbiden_consecutive(line) == ERROR_SYNTAX
 		|| forbiden_char(line) == ERROR_SYNTAX)
-		return (printf("ERROR\n"), ERROR_SYNTAX);
-	return (printf("SUCCESS\n"), SUCCESS);
+		return (ERROR_SYNTAX);
+	return (SUCCESS);
 }
--- src/parsing/token/line_control/line_ctrl_2.c
+++ src/parsing/token/line_control/line_ctrl_2.c
@@ -6,7 +6,7 @@
 /*   By: sguillot <sguillot@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/12 15:51:04 by sguillot          #+#    #+#             */
-/*   Updated: 2024/03/17 16:14:09 by sguillot         ###   ########.fr       */
+/*   Updated: 2024/03/17 17:43:37 by sguillot         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -34,6 +34,22 @@ int	ft_space_ctrl_1(char *line, char cmp_str, char cmp_str2)
 	return (SUCCESS);
 }
 
+int	compare_with_ca(char *l, const char **ca, int *i)
+{
+	int	j;
+
+	j = 0;
+	while (ca[j] && ca[j][0] != l[*i])
+		j++;
+	if (ca[j] && ca[j][0] == l[*i])
+	{
+		if (!l[*i + 1])
+			return (0);
+		(*i)++;
+	}
+	return (j);
+}
+
 int	ft_strcmp_array_space_3(char *l, const char **ca, char *cs)
 {
 	int	i;
@@ -43,25 +59,17 @@ int	ft_strcmp_array_space_3(char *l, const char **ca, char *cs)
 	while (l[++i])
 	{
 		ft_ignore_quotes(l, &i);
-		j = 0;
-		while (ca[j] && ca[j][0] != l[i])
-			j++;
-		if (ca[j] && ca[j][0] == l[i])
+		j = compare_with_ca(l, ca, &i);
+		if (l[i] && ((ft_strlen(cs) == 1 && l[i] != cs[0])
+				|| ft_strlen(cs) == 2))
 		{
-			if (!l[i + 1])
-				break ;
-			i++;
-			if (l[i] && ((ft_strlen(cs) == 1 && l[i] != cs[0])
-					|| ft_strlen(cs) == 2))
-			{
-				while (l[i] && l[i] == ' ')
-					i++;
-				if (l[i] && ft_strlen(cs) == 2 && l[i + 1]
-					&& l[i] == cs[0] && l[i + 1] == cs[1])
-					return (2);
-				else if (ca[j] && ft_strlen(cs) == 1 && l[i] && l[i] == cs[0])
-					return (1);
-			}
+			while (l[i] && l[i] == ' ')
+				i++;
+			if (l[i] && ft_strlen(cs) == 2
+				&& l[i + 1] && l[i] == cs[0] && l[i + 1] == cs[1])
+				return (2);
+			else if (ca[j] && ft_strlen(cs) == 1 && l[i] && l[i] == cs[0])
+				return (1);
 		}
 	}
 	return (SUCCESS);
