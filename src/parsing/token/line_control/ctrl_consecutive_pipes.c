e8a748b4ffb7d5c44b1a14f76332e42bd9a1b31f
GuillotSamuel <samuelguillot75@gmail.com>
2024-03-17 16:34:10 +0100

    correction : consecutive pipes

--- src/parsing/token/line_control/ctrl_consecutive_pipes.c
+++ src/parsing/token/line_control/ctrl_consecutive_pipes.c
@@ -6,30 +6,28 @@
 /*   By: sguillot <sguillot@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/17 13:19:24 by sguillot          #+#    #+#             */
-/*   Updated: 2024/03/17 13:36:26 by sguillot         ###   ########.fr       */
+/*   Updated: 2024/03/17 16:33:08 by sguillot         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../../../includes/minishell.h"
 
-static int	skip_quotes(char *line, int *i)
+static void	ignore_single_quote(char *line, int *i)
 {
-	if (line[*i] == '\"')
-	{
+	(*i)++;
+	while (line[*i] != '\'' && line[*i] != '\0')
 		(*i)++;
-		while (line[*i] != '\0' && line[*i] != '\"')
-			(*i)++;
-	}
-	else if (line[*i] == '\'')
-	{
+	if (line[*i] != '\0')
 		(*i)++;
-		while (line[*i] != '\0' && line[*i] != '\'')
-			(*i)++;
-	}
-	if (line[*i] == '\0')
-		return (ERROR_G);
-	else
-		return (SUCCESS);
+}
+
+static void	ignore_double_quote(char *line, int *i)
+{
+	(*i)++;
+	while (line[*i] != '\"' && line[*i] != '\0')
+		(*i)++;
+	if (line[*i] != '\0')
+		(*i)++;
 }
 
 int	consecutive_pipes_ctrl(char *line)
@@ -39,18 +37,20 @@ int	consecutive_pipes_ctrl(char *line)
 	i = 0;
 	while (line[i] != '\0')
 	{
-		if (skip_quotes(line, &i) == ERROR_G)
-			return (SUCCESS);
+		if (line[i] == '\"')
+			ignore_double_quote(line, &i);
+		else if (line[i] == '\'')
+			ignore_single_quote(line, &i);
 		if (line[i] == '|')
 		{
 			i++;
-			while (line[i] == ' ')
+			while (line[i] == ' ' && line[i] != '\0')
 				i++;
-			if (line[i] != '\0' && line[i] == '|')
+			if (line[i] == '|')
 				return (ft_printf("bash: two pipes in a row `||'\n"),
 					ERROR_SYNTAX);
 		}
-		else
+		if (line[i] != '\0')
 			i++;
 	}
 	return (SUCCESS);
