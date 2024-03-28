3dc23c190082a1d02b4328b593746516036dc1ad
Elouan Mauduit <emauduit@made-f0Dr8s2.clusters.42paris.fr>
2024-03-28 12:58:04 +0100

    petite modif sur la gestion des option de cd

--- src/builtins/cd/check_cd.c
+++ src/builtins/cd/check_cd.c
@@ -6,11 +6,19 @@
 /*   By: emauduit <emauduit@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/28 12:02:24 by emauduit          #+#    #+#             */
-/*   Updated: 2024/03/28 12:05:39 by emauduit         ###   ########.fr       */
+/*   Updated: 2024/03/28 12:57:29 by emauduit         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../../includes/minishell.h"
+
+static int	error_cd(char *arg)
+{
+	ft_putstr_fd("Minishell: cd: ", 2);
+	ft_putstr_fd(arg, 2);
+	ft_putstr_fd(": invalid option\n", 2);
+	return (FAIL);
+}
 
 int	check_cd_option(char **args)
 {
@@ -19,22 +27,10 @@ int	check_cd_option(char **args)
 	i = 1;
 	if (args[0][0] != '\0')
 	{
-		if (args[0][0] == '-')
-		{
-			if (args[0][1] == '-')
-				return (OK);
-			while (args[0][i])
-			{
-				if (args[0][i] != '\0')
-				{
-					ft_putstr_fd("Minishell: cd: ", 2);
-					ft_putstr_fd(args[0], 2);
-					ft_putstr_fd(": invalid option\n", 2);
-					return (-1);
-				}
-				i++;
-			}
-		}
+		if (args[0][0] == '-' && args[0][1] == '\0')
+			return (OK);
+		if (args[0][0] == '-' && args[0][1] != '\0')
+			return (error_cd(args[0]));
 	}
 	return (OK);
 }
